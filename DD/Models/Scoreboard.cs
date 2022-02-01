98f1911962360e13719bcf15b01fd86658aa6269
crkobel <crkobel@verizon.net>
2022-01-31 21:28:40 -0800

    Models for User and Scoreboard

---
+++ DD/Models/Scoreboard.cs
@@ -0,0 +1,11 @@
+namespace Models;
+
+public class Scoreboard {
+    public string UserFirst {get;set;}
+    public string UserSecond {get;set;}
+    public string UserThird {get;set;}
+    public string Username {get;set;}
+    public int GamesPlayed {get;set;}
+    public int GamesWon {get;set;}
+    public int TotalKills {get;set;}
+}
